9c1a7861aca5affd3486950f60a39bd10aac6444
zettydr <96828583+zettydr@users.noreply.github.com>
2022-08-24 11:58:30 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/********************************************************************************************
+* Hello.java
+*********************************************************************************************
+* Diamond Zetty
+* 8/24/2022
+* CSMC 255-004
+********************************************************************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+
+	    System.out.println("Diamond Zetty");
+	    System.out.println("Hello.java");
+	    System.out.println("Says hello to the class");
+	    System.out.println("8/24/2022");
+	    System.out.println("CSMC 255-004");
+	    System.out.println();
+	}
+}
